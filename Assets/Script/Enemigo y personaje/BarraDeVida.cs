3250057acfd9d137483c749699b89760a946e171
amelgates <mirrai2888@gmail.com>
2021-11-28 15:52:27 -0400

    vida

--- Assets/Script/Enemigo y personaje/BarraDeVida.cs
+++ Assets/Script/Enemigo y personaje/BarraDeVida.cs
@@ -9,29 +9,22 @@ public class BarraDeVida : MonoBehaviour
     public static BarraDeVida vida;
     public int vidaMaxima = 100;
     public Slider sliderVida;
-    private int damage = -25;
+    private int damage = -5;
+    public Image vidas;
+    public Sprite[] spriteVidas; 
 
     public void Awake()
     {
+        
+          
         vida = this;
+        vidas = GameObject.Find("vida").GetComponent<Image>();
+        vidas.sprite = spriteVidas[0];
     }
-    public void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Enemigo"))
-        {
-            vidaMaxima = vidaMaxima + damage;
-        }
-    }
-    IEnumerator Example()
-    {
-        Gamemanager.instancia.Showtext("Usas el botiquin, recuperas vida.");
-        yield return new WaitForSecondsRealtime(2f);
-        Gamemanager.instancia.Hidetext();
-    }
-
+    
     public void Update()
     {
-        sliderVida.value = vidaMaxima;
+        //sliderVida.value = vidaMaxima;
         if (Gamemanager.instancia.botiquinMomento && vidaMaxima <= 75)
         {
             vidaMaxima = vidaMaxima + 25;
@@ -42,6 +35,67 @@ public class BarraDeVida : MonoBehaviour
         if (vidaMaxima == 0)
         {
             SceneManager.LoadScene("Pantalla Gameover");
+        }
+        vidas.sprite = cambiarVida();
+        print(vidaMaxima);
+    }
+     public Sprite cambiarVida()
+    {
+        print("hola" + vidaMaxima);
+        //Sprite return spriteVidas [0];
+        if(vidaMaxima>=90)
+        {
+            return spriteVidas[0];
+        }
+        else
+            if (vidaMaxima >= 80 && vidaMaxima < 90)
+            {
+                return spriteVidas[1];
+            }else
+            if (vidaMaxima >= 70 && vidaMaxima < 80)
+            {
+                return spriteVidas[2];
+            }else
+            if (vidaMaxima >= 60 && vidaMaxima < 70)
+            {
+                return spriteVidas[3];
+            }else
+            if (vidaMaxima >= 50 && vidaMaxima < 60)
+            {
+                return spriteVidas[4];
+            }else
+            if (vidaMaxima >= 40 && vidaMaxima < 50)
+            {
+                return spriteVidas[5];
+            }else
+            if (vidaMaxima >= 30 && vidaMaxima < 40)
+            {
+                return spriteVidas[6];
+            }else
+            if (vidaMaxima >= 20 && vidaMaxima < 30)
+            {
+                return spriteVidas[7];
+            }else
+            if (vidaMaxima >= 10 && vidaMaxima < 20)
+            {
+                return spriteVidas[8];
+            }else
+            {
+                return spriteVidas[9];
+            }
+        //return vidaAux;
+    }
+    IEnumerator Example()
+    {
+        Gamemanager.instancia.Showtext("Usas el botiquin, recuperas vida.");
+        yield return new WaitForSecondsRealtime(2f);
+        Gamemanager.instancia.Hidetext();
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemigo"))
+        {
+            vidaMaxima = vidaMaxima + damage;
         }
     }
 }
